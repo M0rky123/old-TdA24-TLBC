984eaa8356d994131238eeb5c5ebdbdad84649ea
M0rky123 <86577843+M0rky123@users.noreply.github.com>
2024-01-03 06:57:54 +0000

    lecturer mobile table responsive

---
+++ app/static/js/lecturer.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var childDivs = document.getElementsByClassName("table-info");
+  var maxWidth = Math.max.apply(
+    null,
+    Array.from(childDivs).map(function (div) {
+      return div.clientWidth;
+    })
+  );
+
+  setTimeout(function () {
+    document.documentElement.style.setProperty("--child-max-width", "0");
+    document.documentElement.style.setProperty("--child-max-width", maxWidth + "px");
+  }, 500);
+});
