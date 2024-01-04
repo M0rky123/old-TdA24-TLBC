0e270f0c8272cd9ddd53e08f692fcf1495e52668
M0rky123 <86577843+M0rky123@users.noreply.github.com>
2024-01-04 06:53:25 +0000

    neivm pico

---
+++ app/static/js/index.js
@@ -0,0 +1,9 @@
+const container = document.getElementsByClassName("tags-container");
+
+// create loop thru all html elements in tags-container
+for (let i = 0; i < container.length; i++) {
+  container[i].addEventListener("wheel", function (e) {
+    container[i].scrollLeft += e.deltaY > 0 ? 100 : -100;
+    e.preventDefault();
+  });
+}
