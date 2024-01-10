a5363f64ae305f558872140b8ec5bb41f9a7b5b3
TheKronis <73104361+TheKronis@users.noreply.github.com>
2024-01-10 12:33:58 +0000

    Update lecture.db, index.css, and posttest.py

--- posttest.py
+++ posttest.py
@@ -5,11 +5,10 @@ url = "http://127.0.0.1:5000/api/lecturers"
 
 # Your JSON data for POST request
 post_data = {
-    "UUID": "8a2b5f91-1c4e-4d7a-9d86-12ab3cde5f12",
     "title_before": "Dr.",
     "first_name": "John",
     "last_name": "Carson",
-    "picture_url": "https://example.com/profile_picture.png",
+    "picture_url": "https://fastly.picsum.photos/id/480/200/200.jpg?hmac=q_kzh_8Ih85_5t_jN3rcD3npeNBLA41oDGtQZVkmmYs",
     "location": "Cityville",
     "claim": "Passionate researcher / Community leader / Tech enthusiast",
     "bio": "I thrive on exploring new ideas and pushing the boundaries of technology. Whether it's conducting research in cutting-edge fields, leading community initiatives, or diving into the world of tech, I am always eager to contribute and learn. My journey has involved everything from developing innovative software solutions to collaborating on open-source projects. Currently pursuing a PhD in Computer Science at Tech University.",
@@ -30,11 +29,20 @@ post_data = {
     }
 }
 
+
+
 # Convert the data to JSON
 json_post_data = json.dumps(post_data)
 
 # Make the POST request
-response_post = requests.post(url, data=json_post_data, headers={"Content-Type": "application/json"})
-print("POST Response:")
-print(response_post.status_code)
-print(response_post.text)
+#response_post = requests.post(url, data=json_post_data, headers={"Content-Type": "application/json"})
+#print("POST Response:")
+#print(response_post.status_code)
+#print(response_post.text)
+
+url = "http://127.0.0.1:5000//api/lecturers/2c556c9b-e00d-403b-ab9d-a8b075024eb0"
+
+response = requests.delete(url)
+print("DELETE Response:")
+print(response.status_code)
+print(response.text)
